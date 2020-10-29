cab58e5dd698c43a4095904e1447ccd929a7ac40
ienvenue <chenyoungjie@163.com>
2020-10-29 08:45:19 +0800

    指环王导出数据添加台数同比

--- export_retail_data.py
+++ export_retail_data.py
@@ -318,6 +318,7 @@ select c.中心, sum(a.总价)/10000 as 销额（万元）,
             and (b.类别 ='3C' or b.类别='TOP') 
             group by c.中心
 '''
+# mmp零售
 sql_mmp2 = '''
 select a.center_group as 分组, a.center_name as 中心, a.target_score as 目标, b.score_1 as 积分, b.number1 as 台数, b.number2 as 高端台数
 from ods.center_group_zhihuanwang a
@@ -352,7 +353,42 @@ group by c.center) b on
 a.center_name = b.center
 where center = '新疆';
     '''
-
+# 19年mmp零售
+sql_19mmp_retail = '''
+select a.center_group as 分组, a.center_name as 中心, a.target_score as 目标, b.score_1 as 积分, b.number1 as 台数, b.number2 as 高端台数
+from ods.center_group_zhihuanwang a
+inner join ( select c.center, sum(b.score * a.数量) as score_1, sum(a.数量) as number1, sum(b.is_jiegouji*a.数量) as number2
+from ods.mmp零售数据 a
+inner join ods.model_score b on
+a.商品编码 = b.merch_id
+inner join ods.area_center_zhihuanwang c on
+c.center_name = a.分部名称
+where ((a.门店一级分类 in ('苏宁', '国美', 'TOP', '五星', '商超')
+and a.门店二级分类 not in ('国美新零售', '苏宁零售云', '五星万镇通'))
+-- 9.29新增v200与部分直营门店
+or (a.门店编码 in ('S00081607', 'S00081632', 'S00081635', 'S00081648', 'S00082093', 'S00091085', 'S00215530', 'S00089426', 'S00081003', 'S00081004', 'S00087008', 'S00191565', 'S00204520', 'S00081031', 'S00081155', 'S00090191', 'S00195186', 'S00081080', 'S00081541', 'S00090768', 'S00090769', 'S00090774', 'S00084355', 'S00084376', 'S00084392', 'S00084393', 'S00081818', 'S00084600', 'S00084648', 'S00078945', 'S00081418', 'S00081424', 'S00081437', 'S00068629', 'S00068639', 'S00013902', 'S00036059', 'S00036060', 'S00102010', 'S00081706', 'S00203272', 'S00081227', 'S00081228', 'S00083873', 'S00014418', 'S00084297', 'S00084755', 'S00088712', 'S00095896', 'S00081376', 'S00081383', 'S00081407', 'S00181472', 'S00239250', 'S00081394', 'S00081411', 'S00081701', 'S00082875', 'S00252523', 'S00081628', 'S00081695', 'S00081702', 'S00081714', 'S00081717', 'S00082886', 'S00253325', 'S00260399', 'S00023693', 'S00076764', 'S00095292', 'S00048922', 'S00270654', 'S00280139')))
+and b.score>0
+and year(a.创建时间) = '2019' 
+group by c.center) b on
+a.center_name = b.center
+where center <> '新疆'
+union all select a.center_group as 分组, a.center_name as 中心, a.target_score as 目标, b.score_1 as 积分, b.number1 as 台数, b.number2 as 高端台数
+from ods.center_group_zhihuanwang a
+inner join ( select c.center, sum(b.score_xinjiang * a.数量) as score_1, sum(a.数量) as number1, sum(b.is_jiegouji*a.数量) as number2
+from ods.mmp零售数据 a
+inner join ods.model_score b on
+a.商品编码 = b.merch_id
+inner join ods.area_center_zhihuanwang c on
+c.center_name = a.分部名称
+where a.门店一级分类 in ('苏宁', '国美', '五星', '商超')
+and a.门店二级分类 not in ('国美新零售', '苏宁零售云', '五星万镇通')
+and b.score_xinjiang >0
+and year(a.创建时间) = '2019'
+group by c.center) b on
+a.center_name = b.center
+where center = '新疆';
+    '''
+# 渠道业务
 sql_mmp3 = '''select
     a.门店一级分类,
     sum(b.score * a.数量) as 积分,
@@ -450,6 +486,7 @@ where
     and year(a.创建时间) = '2020'
 group by
     a.门店一级分类; '''
+# 代理渠道
 sql_mmp4 = '''
 select
 	d.center as 中心,
@@ -519,6 +556,78 @@ where
 group by
 	c.center
 	'''
+
+# 19年代理渠道
+sql_19channel_retail = '''
+select
+	d.center as 中心,
+	sum(d.score*d.`开单数量`) as 积分,
+	sum(d.`开单数量`) 台数,
+	sum(d.is_jiegouji *d.`开单数量`) as 高端台数
+from
+	(
+	select
+		c.center, b.score, a.开单数量,b.is_jiegouji 
+	from
+		ods.二级代理渠道零售数据 a
+	inner join ods.model_score b on
+		a.商品编码 = b.merch_id
+	inner join ods.area_center_zhihuanwang c on
+		c.center_name = a.中心名称
+	where
+	  year(a.单据日期)='2019'  and
+		a.`卖方合作模式大类(CRM)/一级分类(CMDM)` in ('TOP',
+		'V200',
+		'代理商',
+		'多品店',
+		'家装店',
+		'零售代理',
+		'零售商',
+		'其他渠道',
+		'旗舰店',
+		'专卖店',
+		'专业工程',
+		'专业工程代理')
+		and b.score >0
+		and a.卖方客户名称 not like '已失效%'
+union all
+	select
+		c.center, b.score, a.开单数量,b.is_jiegouji 
+	from
+		ods.一级代理渠道零售数据 a
+	inner join ods.model_score b on
+		a.商品编码 = b.merch_id
+		and b.score >0
+		and a.卖方客户名称 not like '已失效%' -- 新增剔除已失效客户
+	inner join ods.area_center_zhihuanwang c on
+		c.center_name = a.中心名称
+		where  year(a.单据日期)='2019' ) d
+where
+	d.center <> '新疆'
+group by
+	d.center
+union all
+select
+	c.center as 中心,
+	sum(b.score_xinjiang * a.数量) as 积分,
+	sum(a.数量) AS 台数,
+	sum(b.is_jiegouji * a.数量) as 高端台数
+from
+	ods.mmp零售数据 a
+inner join ods.model_score b on
+	a.商品编码 = b.merch_id
+inner join ods.area_center_zhihuanwang c on
+	c.center_name = a.分部名称
+where
+	a.门店一级分类 not in ( '苏宁',
+	'国美')
+	and c.center = '新疆'
+	and b.score_xinjiang >0
+	and year(a.创建时间)='2019'
+group by
+	c.center
+	'''
+
 # 今年代理渠道销售台数份额均价
 sql_channel_1 = '''
 select
@@ -815,6 +924,8 @@ df11 = pd.read_sql(sql=sql_channel_2, con=engine)
 df12 = pd.read_sql(sql=sql_channel_3, con=engine)
 df13 = pd.read_sql(sql=sql_mmp_last_month, con=engine)
 df14 = pd.read_sql(sql=sql_colmo, con=engine)
+df15 = pd.read_sql(sql=sql_19mmp_retail, con=engine)
+df16 = pd.read_sql(sql=sql_19channel_retail, con=engine)
 
 
 # write_excel(df=df1,sheet_name='mmp到人员')
@@ -833,6 +944,8 @@ write_excel(df=df11, sheet_name='去年代理渠道销售')
 write_excel(df=df12, sheet_name='上个月代理渠道销售')
 write_excel(df=df13, sheet_name='上个月mmp销售')
 write_excel(df=df14, sheet_name='colmo当月销售')
+write_excel(df=df15, sheet_name='19年MMP')
+write_excel(df=df16, sheet_name='19年代理渠道')
 
 t2 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("指环王模型运行完毕，结束计算时间 :", t2)
