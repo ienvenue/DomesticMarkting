1b3051d80dfcefe86786337de2e2fc555924dc0e
ienvenue <chenyoungjie@163.com>
2020-12-03 09:35:24 +0800

    奥维导入程序

--- GUI_AVC_Data.py
+++ GUI_AVC_Data.py
@@ -2,6 +2,7 @@ from tkinter import ttk,filedialog,messagebox
 import tkinter as tk
 import pandas as pd
 from sqlalchemy import create_engine
+import logging
 import pymysql
 
 # 定义数据库
@@ -25,8 +26,7 @@ engine = create_engine("mysql+pymysql://data_dev:data_dev0.@10.157.2.94:3306/aow
                        '线下价格段', '线下容量段']
 
 月度线下洗衣机 = ['销量', '销额', '单价', '月度', '品牌', '机型', '上市月度', '上市周度', '洗涤类型', '子品牌', '美的渠道', '省份', '洗衣机美的中心',
-                        '通用价格段新', '通用容量段新'
-                        ]
+                        '通用价格段新', '通用容量段新']
 
 周度线下洗衣机 = ['销量', '销额', '单价', '周度', '品牌', '机型', '品类', '变频', '产品类型', '产品系列', '尺寸',
                        '电机类型', '耗电量', '烘干', '烘干类型', '厚度', '能效等级', '品牌类型', '容量', '上市月度', '上市周度',
@@ -78,27 +78,36 @@ entry_file_type.place(x=120, y=190)
 entry_file_type['values']=('xlsx', 'csv')
 
 def avc_month_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header):
-    df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
-    df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
-
+    try:
+        df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
+        df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
+    except Exception as e:
+        logging.exception(e)
 
 def avc_week_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header):
-    df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
-    df['年'] = df['周度'].map(lambda x: x.split('W')[0])
-    df['周'] = df['周度'].map(lambda x: x.split('W')[1])
-    df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
-
+    try:
+        df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
+        df['年'] = df['周度'].map(lambda x: x.split('W')[0])
+        df['周'] = df['周度'].map(lambda x: x.split('W')[1])
+        df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
+    except Exception as e:
+        logging.exception(e)
 
 def avc_month_csv2db(file_path, table_name, type_name, cols, header):
-    df = pd.read_csv(file_path, header=header, usecols=cols, encoding="gbk")
-    df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
-
+    try:
+        df = pd.read_csv(file_path, header=header, usecols=cols, encoding="gbk")
+        df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
+    except Exception as e:
+        logging.exception(e)
 
 def avc_week_csv2db(file_path, table_name, type_name, cols, header):
-    df = pd.read_csv(file_path, header=header, usecols=cols, encoding="gbk", engine='python')
-    df['年'] = df['周度'].map(lambda x: x.split('W')[0])
-    df['周'] = df['周度'].map(lambda x: x.split('W')[1])
-    df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
+    try:
+        df = pd.read_csv(file_path, header=header, usecols=cols, encoding="gbk", engine='python')
+        df['年'] = df['周度'].map(lambda x: x.split('W')[0])
+        df['周'] = df['周度'].map(lambda x: x.split('W')[1])
+        df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
+    except Exception as e:
+        logging.exception(e)
 
 # 绝对路径替换
 def selectPath():
