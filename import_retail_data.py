a8cd5b4aefff27d6ec68d1f37fd56764da7c4fbf
ienvenue <chenyoungjie@163.com>
2021-02-22 11:46:19 +0800

    新增为DF添加当前日期的函数

--- import_retail_data.py
+++ import_retail_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 import pymysql
 from sqlalchemy import create_engine
 
@@ -41,7 +42,7 @@ def del_data(df, tablename):
     conn.commit()
     conn.close()
 
-def save_date(df, tablename,type):
+def save_data(df, tablename,type):
     """
     df存储到数据库
     :param df: 待导入的数据
@@ -51,11 +52,26 @@ def save_date(df, tablename,type):
     engine = create_engine("mysql+pymysql://data_dev:data_dev0.@10.157.2.94:3306/ods")
     df.to_sql(tablename, con=engine, index=False, if_exists=type)
 
-if __name__ == '__main__':
-    mmp_use_col = ['上报ID', '商品型号', '商品编码', '大类', '小类', '门店编码', '门店名称', '门店等级',
-                   '门店一级分类', '门店二级分类', '导购员编码', '导购员名称', '导购员手机号',
-                   '导购类型', '数量', '单价', '零售价', '总价', '资源抵扣金额', '厂家承担券',
-                   '分部名称', 'CMDM中心编码', '产品线', '创建时间', '顾客手机']
+def add_today_column(df,date_str):
+    df['写入日期'] = date_str
 
-    df = pd.read_excel(r'D:\MyData\zhangmd10\Desktop\新建文件夹 (2)\mmp11.2.xlsx', encoding='gbk', usecols=mmp_use_col)
-    del_data(df, 'mmp零售数据全量')
+if __name__ == '__main__':
+    # mmp_use_col = ['上报ID', '商品型号', '商品编码', '大类', '小类', '门店编码', '门店名称', '门店等级',
+    #                '门店一级分类', '门店二级分类', '导购员编码', '导购员名称', '导购员手机号',
+    #                '导购类型', '数量', '单价', '零售价', '总价', '资源抵扣金额', '厂家承担券',
+    #                '分部名称', 'CMDM中心编码', '产品线', '创建时间', '顾客手机']
+    #
+    # df = pd.read_excel(r'D:\MyData\zhangmd10\Desktop\新建文件夹 (2)\mmp11.2.xlsx', encoding='gbk', usecols=mmp_use_col)
+    # del_data(df, 'mmp零售数据全量')
+
+    #导入
+    date_str=str(time.strftime("%Y-%m-%d",time.localtime()))
+    df1 = pd.read_excel(r'\\10.157.2.94\共享文件\Data\进销存方案\内部库存明细.xlsx',sheet_name='库存明细',header=0)
+    add_today_column(df1,date_str)
+    save_data(df1,'洗衣机内部库存明细','append')
+    df2 = pd.read_excel(r'\\10.157.2.94\共享文件\Data\进销存方案\系统未审.xlsx',sheet_name='未审',header=1)
+    add_today_column(df2,date_str)
+    save_data(df2,'洗衣机订单未审明细','append')
+    df3 = pd.read_excel(r'\\10.157.2.94\共享文件\Data\进销存方案\系统未审.xlsx', sheet_name='开提', header=1)
+    add_today_column(df3,date_str)
+    save_data(df3, '洗衣机订单开提明细', 'append')
