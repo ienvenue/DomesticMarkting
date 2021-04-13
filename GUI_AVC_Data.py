a956101e312e5df2b024acc23454df9adb96283d
ienvenue <chenyoungjie@163.com>
2021-04-13 10:23:51 +0800

    奥维中心字段名修改

--- GUI_AVC_Data.py
+++ GUI_AVC_Data.py
@@ -23,21 +23,21 @@ engine = create_engine("mysql+pymysql://data_dev:data_dev0.@10.157.2.94:3306/aow
            '上市月度', '上市周度', '通用价格段', '线上价格段', '小天鹅价格段', '小天鹅容量段', '子品牌'
            ]
 月度线下干衣机 = ['销量', '销额', '单价', '月度', '品牌', '机型', '品类', '变频', '产品类型', '干衣方式', '品牌类型', '容量', '上市月度', '上市周度',
-           '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '洗衣机美的中心', '洗衣机美的中心2', '县市级别2', '价格段',
+           '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '美的中心', '洗衣机美的中心2', '县市级别2', '价格段',
            '容量段', '线下价格段', '线下容量段', '小天鹅价格段', '小天鹅容量段']
 
 月度线下洗衣机 = ['销量', '销额', '单价', '月度', '品牌', '机型', '品类', '壁挂洗衣机', '变频', '产品类型', '产品系列', '尺寸', '电机类型', '耗电量',
            '烘干', '烘干类型', '厚度', '集团品牌', '能效等级', '品牌类型', '容量', '上市月度', '上市周度', '洗涤方式1', '洗涤类型', '小天鹅品牌',
-           '智能', '转速', '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '洗衣机美的中心', '洗衣机美的中心2', '县市级别2',
+           '智能', '转速', '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '美的中心', '洗衣机美的中心2', '县市级别2',
            '通用价格段新', '通用容量段新', '洗涤价格段改', '洗涤容量段改', '小天鹅价格段', '小天鹅容量段']
 
 周度线下洗衣机 = ['销量', '销额', '单价', '周度', '品牌', '机型', '品类', '壁挂洗衣机', '变频', '产品类型', '产品系列', '尺寸', '电机类型', '耗电量',
            '烘干', '烘干类型', '厚度', '集团品牌', '能效等级', '品牌类型', '容量', '上市月度', '上市周度', '洗涤方式1', '洗涤类型', '小天鹅品牌',
-           '智能', '转速', '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '洗衣机美的中心', '洗衣机美的中心2', '县市级别2',
+           '智能', '转速', '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '美的中心', '洗衣机美的中心2', '县市级别2',
            '通用价格段新', '通用容量段新', '洗涤价格段改', '洗涤容量段改', '小天鹅价格段', '小天鹅容量段'
            ]
 周度线下干衣机 = ['销量', '销额', '单价', '周度', '品牌', '机型', '品类', '变频', '产品类型', '干衣方式', '品牌类型', '容量', '上市月度', '上市周度',
-           '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '洗衣机美的中心', '洗衣机美的中心2', '县市级别2', '价格段',
+           '子品牌', '大区', '地市', '美的渠道',  '省份', '洗衣机美的片区', '美的中心', '洗衣机美的中心2', '县市级别2', '价格段',
            '容量段', '线下价格段', '线下容量段', '小天鹅价格段', '小天鹅容量段']
 
 # 第1步，实例化object，建立窗口window
@@ -85,6 +85,7 @@ entry_file_type['values'] = ('xlsx', 'csv')
 def avc_month_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header):
     try:
         df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
+        df.rename(columns={'美的中心': '洗衣机美的中心'},inplace=True)
         df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
         messagebox.showinfo('提示', '导入成功')
     except Exception as e:
@@ -94,6 +95,7 @@ def avc_month_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header
 def avc_week_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header):
     try:
         df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, usecols=cols)
+        df.rename(columns={'美的中心':'洗衣机美的中心'},inplace=True)
         df['年'] = df['周度'].map(lambda x: x.split('W')[0])
         df['周'] = df['周度'].map(lambda x: x.split('W')[1])
         df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
@@ -105,6 +107,7 @@ def avc_week_xlsx2db(file_path, table_name, sheet_name, type_name, cols, header)
 def avc_month_csv2db(file_path, table_name, type_name, cols):
     try:
         df = pd.read_csv(file_path, usecols=cols, encoding="gbk")
+        df.rename(columns={'美的中心': '洗衣机美的中心'}, inplace=True)
         df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
         messagebox.showinfo('提示', '导入成功')
     except Exception as e:
@@ -114,6 +117,7 @@ def avc_month_csv2db(file_path, table_name, type_name, cols):
 def avc_week_csv2db(file_path, table_name, type_name, cols):
     try:
         df = pd.read_csv(file_path, usecols=cols, encoding="gbk", engine='python')
+        df.rename(columns={'美的中心': '洗衣机美的中心'}, inplace=True)
         df['年'] = df['周度'].map(lambda x: x.split('W')[0])
         df['周'] = df['周度'].map(lambda x: x.split('W')[1])
         df.to_sql(name=table_name, con=engine, if_exists=type_name, index=False)
